38a0ddd313ca68c65065fe7000c88c41707c7e32
ziauldin123 <mziauldin2@gmail.com>
2022-05-08 15:42:00 +0500

    first commit

--- .platform/hooks/postdeploy/-01_django.sh
+++ .platform/hooks/postdeploy/-01_django.sh
@@ -7,9 +7,14 @@ source /var/app/venv/*/bin/activate && {
 
 # collecting static files
 python manage.py collectstatic --noinput;
+
 # log which migrations have already been applied
 python manage.py makemigrations --noinput;
+
 # migrate the rest
 python manage.py migrate --noinput;
+
 # another command to create a superuser (write your own)
+python manage.py mysuperuser --noinput;
+
 }
